3d5243c26637497912d9eccdd1ccd45d168468d3
b-enedict <benedict.seuss@gmail.com>
2024-11-30 23:28:57 +0100

    Make design nicer

--- frontend/src/components/topBar/topBar.js
+++ frontend/src/components/topBar/topBar.js
@@ -7,7 +7,7 @@ export default function TopBar() {
     const [isAuthenticated, setIsAuthenticated] = useState(checkAuth())
 
     return (
-        <div className='flex justify-between items-center w-full topbar py-5 px-10 gap-x-7'>
+        <div className="flex justify-between items-center w-full topbar py-5 px-10 gap-x-7 bg-gradient-to-r from-pink-500 to-purple-600 text-white">
         <div>STEPZZ</div>
         {isAuthenticated && <Dropdown setIsAuthenticated={setIsAuthenticated} /> }
         {!isAuthenticated && <Link href="/login">Login</Link>}

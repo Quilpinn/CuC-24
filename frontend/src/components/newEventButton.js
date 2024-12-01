204a5d2c254ff0a7e5953dcc5472e15ed9ee77e2
Ines Zenkri <ines@zenkri.com>
2024-12-01 09:05:16 +0000

    button

--- frontend/src/components/newEventButton.js
+++ frontend/src/components/newEventButton.js
@@ -1,6 +1,5 @@
 import { checkAuth } from "@/services/cookies";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function NewEventButton() {
     const isAuthenticated = checkAuth();
@@ -8,7 +7,8 @@ export default function NewEventButton() {
     return (
         <Link href="/newPost">
             <div
-                className="z-10 fixed bottom-5 right-12 bg-gradient-to-r from-pink-500 to-purple-600 shadow-lg p-5 rounded-full flex items-center text-white transition-all duration-300 ease-in-out group hover:pr-6 hover:pl-5 hover:rounded-xl"
+                className="z-10 fixed bottom-5 right-12 bg-gradient-to-r from-pink-500 to-purple-600 shadow-lg p-4 rounded-full flex items-center text-white group transition-all duration-300 ease-in-out hover:pr-6 hover:pl-5 hover:rounded-lg"
+                style={{ width: 'auto' }}
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +27,7 @@ export default function NewEventButton() {
                     <path d="M5 12l14 0" />
                 </svg>
                 <span
-                    className="ml-3 opacity-0 group-hover:opacity-100 group-hover:ml-4 transition-all duration-300 ease-in-out"
+                    className="ml-2 whitespace-nowrap opacity-0 group-hover:opacity-100 group-hover:ml-4 transition-all duration-300 ease-in-out overflow-hidden"
                 >
                     Neues Event anlegen
                 </span>
